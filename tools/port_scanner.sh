3eea5a3ffd74e55c3455ad897c261ca36c57eb42
isjsm <84210598+isjsm@users.noreply.github.com>
2025-03-09 17:25:00 +0300

    Update port_scanner.sh

--- tools/port_scanner.sh
+++ tools/port_scanner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "أداة مسح المنافذ باستخدام Nmap"
-read -p "أدخل عنوان IP: " ip
+echo "Port Scanner By namp"
+read -p "Enter Website Ip: " ip
 nmap -sV $ip
-read -p "اضغط Enter للعودة إلى القائمة..."
+read -p "Chose Enter To Exit"
 ./toolkit.sh
