ffb2589e902f62f1ea217fc7a005e5bb9d4b01dc
isjsm <84210598+isjsm@users.noreply.github.com>
2025-03-09 17:46:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 # تثبيت الاعتمادات
 sudo apt update
 sudo apt install -y figlet boxes nmap
 
-# إنشاء روابط رمزية في /usr/local/bin
-sudo ln -sf $(pwd)/toolkit.sh /usr/local/bin/mytool
-sudo chmod +x /usr/local/bin/mytool
+# إنشاء رابط رمزي
+sudo ln -sf $(pwd)/toolkit.sh /usr/local/bin/cyper-tool
+sudo chmod +x /usr/local/bin/cyper-tool
 
-echo -e "\nتم التثبيت! يمكنك الآن استخدام الأمر: mytool"
+echo -e "\nInstallation complete! Use: cyper-tool"
